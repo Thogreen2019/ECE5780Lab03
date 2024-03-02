8b122f50d6bc6ad48ef64621592adb14ac8f2e25
Thogreen2019 <60160577+Thogreen2019@users.noreply.github.com>
2024-03-02 00:38:33 -0700

    Update main.c
    Working model after changing order of code and making minor syntax changes. No, I don't know why this suddenly works.

--- Lab03/Timer/Core/Src/main.c
+++ Lab03/Timer/Core/Src/main.c
@@ -42,32 +42,25 @@ int main(void)
 	RCC->APB1ENR |= RCC_APB1ENR_TIM2EN;
 	RCC->APB1ENR |= RCC_APB1ENR_TIM3EN;
 	
-	//Set up LEDs
-	GPIOC -> MODER &= (0<<19); //Set Green LED to Output
-	GPIOC -> MODER |= (1<<18); //Set Green LED to Output
-	GPIOC -> OTYPER &= (0<<9); //Set Green LED to no push/pull
-	GPIOC -> OSPEEDR &= (0<<18); //Set Green LED to Low Speed
-	GPIOC -> PUPDR &= (0<<18); //Set Green LED to no Pull up/down
+	//Set Up Red LED
+	GPIOC->MODER &= ~(1<<12); //Set to alternte function mode
+	GPIOC->MODER |= (1<<13);
+	GPIOC->OTYPER &= ~(1<<6); //Set to push-pull mode
+	GPIOC->OSPEEDR &= ~(1<<12); //Set to low speed
+	GPIOC->OSPEEDR &= ~(1<<13);
+	GPIOC->PUPDR &= ~(1<<12); //Set no pull-up/down
+	GPIOC->PUPDR &= ~(1<<13);
+
+	//Set Up Blue LED
+	GPIOC->MODER &= ~(1<<14); //Set to alternate function mode
+	GPIOC->MODER |= (1<<15);
+	GPIOC->OTYPER &= ~(1<<7); //Set to push-pull mode
+	GPIOC->OSPEEDR &= ~(1<<14); //Set to low speed
+	GPIOC->OSPEEDR &= ~(1<<15);
+	GPIOC->PUPDR &= ~(1<<14); //Set no pull-up/down
+	GPIOC->PUPDR &= ~(1<<15);
 	
-	GPIOC -> MODER &= (0<<17); //Set Green LED to Output
-	GPIOC -> MODER |= (1<<16); //Set Orange LED to Output
-	GPIOC -> OTYPER &= (0<<8); //Set Orange LED to no push/pull
-	GPIOC -> OSPEEDR &= (0<<16); //Set Orange LED to Low Speed
-	GPIOC -> PUPDR &= (0<<16); //Set Orange LED to no Pull up/down
-	
-	GPIOC -> MODER |= (1<<13); //Set Red LED to Alternate Output
-	GPIOC -> MODER &= (0<<12); //Set Red LED to Alternate Output
-	GPIOC -> OTYPER &= (0<<6); //Set Red LED to no push/pull
-	GPIOC -> OSPEEDR &= (0<<12); //Set Red LED to Low Speed
-	GPIOC -> PUPDR &= (0<<12); //Set Red LED to no Pull up/down
-	
-	GPIOC -> MODER |= (1<<15); //Set Blue LED to Alternate Output
-	GPIOC -> MODER &= (0<<14); //Set Red LED to Alternate Output
-	GPIOC -> OTYPER &= (0<<7); //Set Blue LED to no push/pull
-	GPIOC -> OSPEEDR &= (0<<14); //Set Blue LED to Low Speed
-	GPIOC -> PUPDR &= (0<<14); //Set Blue to no Pull up/down
-	
-	//Set up the Alternate Function of the Red and Blue LEDS (GPIO AFR)
+	//Set Up GPIOC AFR for Red and Blue LEDS to TIM3
 	GPIOC->AFR[0] &= ~(1<<24);
 	GPIOC->AFR[0] &= ~(1<<25);
 	GPIOC->AFR[0] &= ~(1<<26);
@@ -77,61 +70,73 @@ int main(void)
 	GPIOC->AFR[0] &= ~(1<<30);
 	GPIOC->AFR[0] &= ~(1<<31);
 	
-	GPIOC -> ODR |= (1<<9); //Set Green LED to on
-	GPIOC -> ODR |= (1<<7); //Set Blue LED to on
-	GPIOC -> ODR |= (1<<6); //Set Red LED to on
+	//Set Up Orange LED
+	GPIOC->MODER |= (1<<16); //Set to output mode
+	GPIOC->MODER &= ~(1<<17);
+	GPIOC->OTYPER &= ~(1<<8); //Set to push-pull mode
+	GPIOC->OSPEEDR &= ~(1<<16); //Set to low speed
+	GPIOC->OSPEEDR &= ~(1<<17);
+	GPIOC->PUPDR &= ~(1<<16); //Set no pull-up/down
+	GPIOC->PUPDR &= ~(1<<17);
 	
-	//Next, configure the timer (UEV) to update at 4Hz
-	//4Hz = fclk/((PSC+1)*ARR), fclk = 8MHz, PSC + 1 = 8000 -> PSC = 7999
-	//-> 4Hz = 1kHz/ARR -> ARR = 250
-	TIM2 -> PSC |= 7999;
-	TIM2 -> ARR |= 250;
+	//Set Up Green LED
+	GPIOC->MODER |= (1<<18); //Set to output mode
+	GPIOC->MODER &= ~(1<<19);
+	GPIOC->OTYPER &= ~(1<<9); //Set to push-pull mode
+	GPIOC->OSPEEDR &= ~(1<<18); //Set to low speed
+	GPIOC->OSPEEDR &= ~(1<<19);
+	GPIOC->PUPDR &= ~(1<<18); //Set no pull-up/down
+	GPIOC->PUPDR &= ~(1<<19);
 	
-	//Configure TIM3 to have UEV of 800Hz
-	// 800Hz = 8MHz/(8*ARR) -> ARR = 1000000/800 = 125
-	TIM3 -> PSC |= 7;
-	TIM3 -> ARR |= 125;
+	GPIOC->ODR |= (1<<6); //Set Red LED to on
+	GPIOC->ODR |= (1<<7); //Set Blue LED to on
+	GPIOC->ODR &= ~(1<<8); //Set Orange LED to off
+	GPIOC->ODR |= (1<<9); //Set Green LED to on
 	
-	//Clear bits 8/9 and 0/1 of TIM3 -> CCMR1 to ensure it is in output mode
-	TIM3 -> CCMR1 &= (0<<9);
-	TIM3 -> CCMR1 &= (0<<8);
+	//TIM2 needs to be set to 4Hz starting at 8MHz
+	//Set PSC to 7999 to get the clock freqency to 1kHz
+	//Multiply new clock frequency by 250 to get 4Hz
+	TIM2->PSC = 7999;
+	TIM2->ARR = 250;
 	
-	TIM3 -> CCMR1 &= (0<<1);
-	TIM3 -> CCMR1 &= (0<<0);
+	TIM2->DIER |= (1<<0); // Enable the update interrupt
 	
-	//Set bits 4,5, 6 of TIM3 -> CCMR1 to PWM2 (111)
-	TIM3 -> CCMR1 |= (1<<4);
-	TIM3 -> CCMR1 |= (1<<5);
-	TIM3 -> CCMR1 |= (1<<6);
+	//TIM3 needs to be set to 800Hz starting from 8MHz
+	//Set PSC to 7 to get the clock frequency to 100MHz
+	//Set ARR to 1250 to get 800Hz
+	TIM3->PSC = 7;
+	TIM3->ARR = 1250;
 	
-	//Set bits 12,13, 14 of TIM3 -> CCMR1 to PWM1 (110)
-	TIM3 -> CCMR1 &= (0<<12);
-	TIM3 -> CCMR1 |= (1<<13);
-	TIM3 -> CCMR1 |= (1<<14);
+	//Set output channel 1 to PWM Mode 2
+	TIM3->CCMR1 |= (1<<4);
+	TIM3->CCMR1 |= (1<<5);
+	TIM3->CCMR1 |= (1<<6);
 	
-	//Enable the Compare Preload Enables of TIM3 (bits 3 and 11)
-	//TIM3 -> CCMR1 |= (1<<3);
-	//TIM3 -> CCMR1 |= (1<<11);
+	//Enable output compare preload for channel 1
+	TIM3->CCMR1 |= (1<<3);
 	
-	//Enable outputs of Channels 1 and 2 in TIM3, register CCER (bits 0 and 4)
-	TIM3 -> CCER |= 1;
-	TIM3 -> CCER |= (1<<4);
+	//Set output channel 2 to PWM Mode 1
+	TIM3->CCMR1 &= ~(1<<12);
+	TIM3->CCMR1 |= (1<<13);
+	TIM3->CCMR1 |= (1<<14);
 	
-	//Set CCRx registers of TIM3 to 50
-	TIM3 -> CCR1 |= 25;
-	TIM3 -> CCR2 |= 25;
+	//Enable output compare preload for channel 2
+	TIM3->CCMR1 |= (1<<11);
 	
-	//Next, Configure the Timer's DMA to update the interrupt (bit 0 of TIMx->DIER):
-	TIM2 -> DIER |= 1;
+	//Set the output enable bits for channels 1 and 2
+	TIM3->CCER |= (1<<0); // Channel 1
+	TIM3->CCER |= (1<<4); // Channel 2
 	
-	//Enable the Interrupts from the timer
+	//Set the CCRx to 20% of ARR (1250*0.2 = 250)
+	TIM3->CCR1 = 250;
+	TIM3->CCR2 = 250;
+	
+	// Enable EXTI interrupt
 	NVIC_EnableIRQ(TIM2_IRQn);
 	
-	//Set Interrupt's Priority
-	//NVIC_SetPriority(15, 1);
-	//Enable Timer (Using bit 0 of the CR1 Register). Note that this is done last.
-	TIM2 -> CR1 |= 1;
-	TIM3 -> CR1 |= 1;
+	// Enable CR1 register of Timer (done last)
+	TIM2->CR1 |= (1<<0);
+	TIM3->CR1 |= (1<<0);
 	
   while (1)
   {
